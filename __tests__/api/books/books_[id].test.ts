81d91decc70251956361edc971b03c3ba261b82b
Arthur Giroux <arthurgiroux@gmail.com>
2024-09-04 22:21:57 -0600

    Fixed formatting.

--- __tests__/api/books/books_[id].test.ts
+++ __tests__/api/books/books_[id].test.ts
@@ -6,63 +6,65 @@ import { GET, DELETE } from "@/app/api/books/[id]/route";
 import { prismaMock } from "@/__mocks__/prisma";
 
 describe("Books API", () => {
-    it("should get a book", async () => {
-        const book: any = {
-            id: "1",
-            title: "Test Book",
-            authorId: "123",
-            isbn: "9781234567897",
-        };
+  it("should get a book", async () => {
+    const book: any = {
+      id: "1",
+      title: "Test Book",
+      authorId: "123",
+      isbn: "9781234567897",
+    };
 
-        prismaMock.book.findUnique.mockResolvedValueOnce(book);
+    prismaMock.book.findUnique.mockResolvedValueOnce(book);
 
-        const response = await GET({} as any, { params: { id: "1" } });
-        const body = await response.json();
-        expect(response.status).toBe(200);
-        expect(body).toEqual(book);
-    });
+    const response = await GET({} as any, { params: { id: "1" } });
+    const body = await response.json();
+    expect(response.status).toBe(200);
+    expect(body).toEqual(book);
+  });
 
-    it("should return 404 if book not found", async () => {
-        prismaMock.book.findUnique.mockResolvedValueOnce(null);
+  it("should return 404 if book not found", async () => {
+    prismaMock.book.findUnique.mockResolvedValueOnce(null);
 
-        const response = await GET({} as any, { params: { id: "1" } });
-        const body = await response.json();
-        expect(response.status).toBe(404);
-        expect(body).toEqual({ message: "Book not found" });
-    });
+    const response = await GET({} as any, { params: { id: "1" } });
+    const body = await response.json();
+    expect(response.status).toBe(404);
+    expect(body).toEqual({ message: "Book not found" });
+  });
 
-    it("should return 500 if an error occurs while getting a book", async () => {
-        prismaMock.book.findUnique.mockRejectedValueOnce(new Error("Unknown error"));
+  it("should return 500 if an error occurs while getting a book", async () => {
+    prismaMock.book.findUnique.mockRejectedValueOnce(
+      new Error("Unknown error")
+    );
 
-        const response = await GET({} as any, { params: { id: "1" } });
-        const body = await response.json();
-        expect(response.status).toBe(500);
-        expect(body).toEqual({ message: "Error getting book" });
-    });
+    const response = await GET({} as any, { params: { id: "1" } });
+    const body = await response.json();
+    expect(response.status).toBe(500);
+    expect(body).toEqual({ message: "Error getting book" });
+  });
 
-    it("should delete a book", async () => {
-        const book: any = {
-            id: "1",
-            title: "Test Book",
-            authorId: "123",
-            isbn: "9781234567897",
-        };
+  it("should delete a book", async () => {
+    const book: any = {
+      id: "1",
+      title: "Test Book",
+      authorId: "123",
+      isbn: "9781234567897",
+    };
 
-        prismaMock.book.delete.mockResolvedValueOnce(book);
+    prismaMock.book.delete.mockResolvedValueOnce(book);
 
-        const response = await DELETE({} as any, { params: { id: "1" } });
-        const body = await response.json();
-        expect(prismaMock.book.delete).toHaveBeenCalledWith({ where: { id: "1" } });
-        expect(response.status).toBe(200);
-        expect(body).toEqual(book);
-    });
+    const response = await DELETE({} as any, { params: { id: "1" } });
+    const body = await response.json();
+    expect(prismaMock.book.delete).toHaveBeenCalledWith({ where: { id: "1" } });
+    expect(response.status).toBe(200);
+    expect(body).toEqual(book);
+  });
 
-    it("should return 500 if an error occurs while deleting a book", async () => {
-        prismaMock.book.delete.mockRejectedValueOnce(new Error("Unknown error"));
+  it("should return 500 if an error occurs while deleting a book", async () => {
+    prismaMock.book.delete.mockRejectedValueOnce(new Error("Unknown error"));
 
-        const response = await DELETE({} as any, { params: { id: "1" } });
-        const body = await response.json();
-        expect(response.status).toBe(500);
-        expect(body).toEqual({ message: "Error deleting book" });
-    });
+    const response = await DELETE({} as any, { params: { id: "1" } });
+    const body = await response.json();
+    expect(response.status).toBe(500);
+    expect(body).toEqual({ message: "Error deleting book" });
+  });
 });
