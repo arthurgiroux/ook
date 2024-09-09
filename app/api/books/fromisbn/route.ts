427fe0f6934d90cb4f5220326ffb1aa23649c116
Arthur Giroux <arthurgiroux@gmail.com>
2024-09-08 22:08:36 -0600

    Added logic to download cover image.

--- app/api/books/fromisbn/route.ts
+++ app/api/books/fromisbn/route.ts
@@ -1,4 +1,48 @@
 import prisma from "@/lib/prisma";
+import path from "path";
+import fetch from "node-fetch";
+import fs from "fs";
+
+const allowedImageTypes: Record<string, string[]> = {
+  "image/jpeg": [".jpeg", ".jpg"],
+  "image/png": [".png"],
+  "image/gif": [".gif"],
+};
+
+function isExtensionValid(filename: string, contentType: string): boolean {
+  const ext = path.extname(filename).toLowerCase();
+  return allowedImageTypes[contentType]?.includes(ext) ?? false;
+}
+
+async function downloadCoverImage(
+  url: string,
+  outputFileName: string
+): Promise<void> {
+  const fileName = path.basename(url);
+  const outputFileNameWithExt = `${outputFileName}${path.extname(fileName).toLowerCase()}`;
+  const outputImagePath = path.join(
+    process.cwd(),
+    "public",
+    "images",
+    "cover",
+    outputFileNameWithExt
+  );
+
+  try {
+    const response = await fetch(url);
+    const contentType = response.headers.get("content-type");
+
+    if (contentType && isExtensionValid(fileName, contentType)) {
+      const buffer = await response.arrayBuffer();
+
+      fs.writeFileSync(outputImagePath, Buffer.from(buffer));
+    } else {
+      console.log(`Invalid extension for image, {}`);
+    }
+  } catch (error) {
+    console.log(`Error while downloading cover image: {error}`);
+  }
+}
 
 export async function POST(request: Request) {
   try {
@@ -9,7 +53,7 @@ export async function POST(request: Request) {
       `http://openlibrary.org/api/volumes/brief/isbn/${isbn}.json`
     );
 
-    const data = await response.json();
+    const data = (await response.json()) as any;
     if (data?.records) {
       // Note: openlibrary has a very strange data model where the details are nested inside the details object
       const firstRecordDetails = (Object.values(data.records)[0] as any)
@@ -47,6 +91,13 @@ export async function POST(request: Request) {
             authorId: authorId.id,
           },
         });
+
+        const firstRecordData = (Object.values(data.records)[0] as any)?.data;
+        const mediumCoverUrl = firstRecordData.cover?.medium;
+        if (mediumCoverUrl) {
+          console.log(`Downloading cover image ${mediumCoverUrl}`);
+          await downloadCoverImage(mediumCoverUrl, book.id);
+        }
 
         return new Response(JSON.stringify(book), { status: 201 });
       }
