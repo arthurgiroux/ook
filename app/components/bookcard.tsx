f3b2aec4ae326294d8262aa9ef17612ad6b1f7c3
Arthur Giroux <arthurgiroux@gmail.com>
2024-09-17 16:48:55 -0600

    Refactored book card component.

---
+++ app/components/bookcard.tsx
@@ -0,0 +1,31 @@
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Typography,
+} from "@material-tailwind/react";
+import React from "react";
+
+interface BookCardProps {
+  id: number;
+  title: string;
+  author: string;
+}
+
+export default function BookCard({ id, title, author }: BookCardProps) {
+  return (
+    <Card className="hover:bg-gray-800 bg-gray-900 p-2 m-4 cursor-pointer">
+      <CardHeader className="bg-gray-900" floated={false}>
+        <img src="https://placehold.co/250x250" alt="card-image" />
+      </CardHeader>
+      <CardBody>
+        <Typography variant="h5" color="white" className="mb-2">
+          {title}
+        </Typography>
+        <Typography className="text-gray-500">{author}</Typography>
+      </CardBody>
+    </Card>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,15 @@
 import { Button } from "@material-tailwind/react";
 import Sidebar from "@/app/components/sidebar";
 import { Input } from "@material-tailwind/react";
+import BookCard from "./components/bookcard";
 
 export default function Page() {
   return (
-  <>
-        <div className="flex h-full bg-gray-900">
+    <>
+      <div className="flex h-full bg-gray-900">
         <Sidebar />
-         {/* Main Content */}
-         <main className="flex-1 p-6 h-screen overflow-y-auto">
+        {/* Main Content */}
+        <main className="flex-1 p-6 h-screen overflow-y-auto">
           {/* Search bar */}
           <div className="mb-6">
             <Input
@@ -23,23 +24,14 @@ export default function Page() {
           {/* Book Cards Grid */}
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
             {/* Example Book Card */}
-            {Array(10).fill(0).map((_, i) => (
-              <div key={i} className="bg-gray-800 p-4 rounded-lg shadow-md">
-                <div className="aspect-w-1 aspect-h-1">
-                  <img
-                    className="w-full h-full object-cover rounded-md"
-                    src="https://placehold.co/200x300"
-                    alt="Book Cover"
-                  />
-                </div>
-                <div className="mt-4">
-                  <h3 className="text-lg font-medium text-gray-100">Book Title</h3>
-                  <p className="mt-1 text-sm text-gray-400">Author</p>
-                </div>
-              </div>
-            ))}
+            {Array(10)
+              .fill(0)
+              .map((_, i) => (
+                <BookCard id={i} author="Author" title="Title" />
+              ))}
           </div>
         </main>
-        </div>
-  </>);
+      </div>
+    </>
+  );
 }
